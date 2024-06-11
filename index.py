16c8dee5ff4d23b765a3fe4c9e44b187ca0b831c
DavidCRandall <142061108+ilovepi31415@users.noreply.github.com>
2024-06-11 17:03:25 -0400

    Upgrade Class and Locked Buttons
    I began work on a new Button Class for the Upgrade paths. This included an unclickable locked button state

--- index.py
+++ index.py
@@ -7,7 +7,8 @@ import pygame
 filename = 'savefile.json'
 data = {  # Defaults
     'score': 0,
-    'upgrades_1': [0, 0, 0, 0],
+    'upgrades': [0, 0, 0, 0],
+    'upgrade_unlocks' : [True, True, True, True],
     'points_per_click': 1
 }
 
@@ -17,8 +18,9 @@ with open(filename, 'r') as file:
 
 # Assign JSON values to python variables
 score = data['score']
-upgrades = data['upgrades_1']
+upgrades = data['upgrades']
 points_per_click = data['points_per_click']
+unlocks = data['upgrade_unlocks']
 
 class Button:
     def __init__(self, x, y, image, clicked, scale = 1):
@@ -42,6 +44,29 @@ class Button:
             self.clicked = False
         return False
 
+class Upgrade(Button):
+    def __init__(self, x, y, image, clicked, title, price, scale = 1, locked = True):
+        super().__init__(x, y, image, clicked, scale)
+        self.locked_image = pygame.image.load('icons/button_locked.png')
+        width = self.locked_image.get_width()
+        height = self.locked_image.get_height()
+        self.locked_image = pygame.transform.scale(self.locked_image, (width * scale, height * scale))
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.title = title
+        self.price = price
+        self.locked = locked
+
+    def draw(self):
+        if not self.locked:
+            return super().draw()
+        else:
+            screen.blit(self.locked_image, (self.rect.x, self.rect.y))
+
+
+
+
+
+
 def get_price(tier):
     level = upgrades[tier - 1]
     if level < 6:
@@ -54,7 +79,7 @@ def format_big_number(n):
     if n < 1000: return n
     symbols = ['', 'K', 'M', 'B', 'T', 'q', 'Q', 's', 'S', 'O', 'N', 'd', 'Ud', 'Dd', 'Td', 'qd', 'Qd', 'sd', 'Sd', 'Od', 'Nd', 'V']
     orders_of_magnitude = int(math.log(n, 10) / 3)
-    small = int(n / (1000 ** orders_of_magnitude) * 10) / 10
+    small = int(n / (1000 ** orders_of_magnitude) * 100) / 100
     if orders_of_magnitude < len(symbols):
         return str(small) + symbols[orders_of_magnitude]
     else: return str(small) + 'e' + str(orders_of_magnitude * 3)
@@ -64,15 +89,16 @@ def format_big_number(n):
 pygame.init()
 screen = pygame.display.set_mode((800, 500))
 pygame.display.set_caption('Clicker Game v0.1')
-pixel_font = pygame.font.Font('pixelType.ttf', 50)
+title_font = pygame.font.Font('pixelType.ttf', 70)
+body_font = pygame.font.Font('pixelType.ttf', 40)
 clock = pygame.time.Clock()
 
 main_button = Button(70, 200, pygame.image.load('icons/score_button.png').convert_alpha(), pygame.image.load(
     'icons/score_clicked.png').convert_alpha(), 10)
 clear_button = Button(500, 200, pygame.image.load('icons/clear_button.png').convert_alpha(), pygame.image.load(
     'icons/clear_clicked.png').convert_alpha(), 5)
-upgrade_button = Button(500, 100, pygame.image.load('icons/upgrade_button.png').convert_alpha(), pygame.image.load(
-    'icons/upgrade_clicked.png').convert_alpha(), 5)
+upgrade_button = Upgrade(500, 100, pygame.image.load('icons/upgrade_button.png').convert_alpha(), pygame.image.load(
+    'icons/upgrade_clicked.png').convert_alpha(), 'hi', 2, 5, unlocks[0])
 
 
 # Game Loop goes here
@@ -89,18 +115,22 @@ while run:
         score = 0
         points_per_click = 1
         upgrades = [0, 0, 0, 0]
+        unlocks = [True, True, True, True]
     if upgrade_button.draw():
         price = get_price(1)
         if price <= score:
             score -= price
             upgrades[0] += 1
             points_per_click += 1
+    if score >= 10 and upgrade_button.locked:
+        unlocks[0] = False
+    upgrade_button.locked = unlocks[0]
 
-    score_surf = pixel_font.render(f'Score: {format_big_number(score)}', False, (255, 255, 255))
+    score_surf = title_font.render(f'$ {format_big_number(score)}', False, (255, 255, 255))
     score_rect = score_surf.get_rect(center=(400, 350))
     screen.blit(score_surf, score_rect)
 
-    price_surf = pixel_font.render(f'Price: {format_big_number(get_price(1))}', False, (255, 255, 255))
+    price_surf = body_font.render(f'Price: $ {format_big_number(get_price(1))}', False, (255, 255, 255))
     price_rect = price_surf.get_rect(center=(400, 250))
     screen.blit(price_surf, price_rect)
 
@@ -110,8 +140,9 @@ while run:
 pygame.quit()
 
 data['score'] = score
-data['upgrades_1'] = upgrades
+data['upgrades'] = upgrades
 data['points_per_click'] = points_per_click
+data['upgrade_unlocks'] = unlocks
 
 with open(filename, 'w') as file:
     json.dump(data, file)
