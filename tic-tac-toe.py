b8df774164a34946d756a4ad2b5abca37f2e2c5e
David Randall <davidra@andrews.edu>
2025-11-03 22:37:41 -0500

    Add better AI to the game

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,27 +1,101 @@
+from random import randint
+
+PLAYER = 1
+AI = 2
+
 class GameBoard():
     markers = [' ', 'X', 'O']
 
     def __init__(self):
         self.state = [0] * 9
         self.human_turn = True
+        self.turn = 0
 
-    def place(self, tile):
-        self.state[tile] = 1
+    def place(self, tile, player):
+        self.state[tile] = player
+        self.turn += 1
 
     def move_human(self):
+        self.human_turn = False
         move = None
-        while not move:
-            move = input("Move: ")
-        self.place(int(move))
+        while not isinstance(move, int) or self.state[move]:
+            move = int(input("Move: "))
+        self.place(move, PLAYER)
 
-    def move_ai(self):
-        pass
+    def move_ai_random(self):
+        move = None
+        while not isinstance(move, int) or self.state[move]:
+            move = randint(0, 8)
+        self.place(move, AI)
+        self.human_turn = True
+
+    def move_ai_naive(self):
+        self.human_turn = True
+        for i in range(len(self.state)):
+            if self.state[i] == 0:
+                self.state[i] = AI
+                if self.check_winner(AI):
+                    self.place(i, AI)
+                    return
+                self.state[i] = 0
+        for i in range(len(self.state)):
+            if self.state[i] == 0:
+                self.state[i] = PLAYER
+                if self.check_winner(PLAYER):
+                    self.place(i, AI)
+                    return
+                self.state[i] = 0
+        self.move_ai_random()
+    
+    def move_ai_perfect(self):
+        self.human_turn = True
+        if self.turn == 0:
+            self.place(5, AI)
+            return
+        elif self.turn == 1:
+            if self.state[4]:
+                self.place(0, AI)
+            else:
+                self.place(4, AI)
+            return
+        elif self.turn == 3 and self.state[4] == AI:
+            if (
+                self.state[0] and self.state[8] or
+                self.state[2] and self.state[6]
+            ):
+                self.place(1, AI)
+                return
+            elif not (self.state[1] or self.state[7]):
+                self.place(7, AI)
+                return
+            elif not (self.state[3] or self.state[5]):
+                self.place(3, AI)
+                return
+        self.move_ai_naive()
 
     def display(self):
         for row in range(3):
             for col in range(3):
                 print(GameBoard.markers[self.state[row*3 + col]], end='')
             print()
+        print()
+
+    def check_winner(self, player):
+        return (
+                # horizontal row
+                self.state[0] == self.state[1] == self.state[2] == player or
+                self.state[3] == self.state[4] == self.state[5] == player or
+                self.state[6] == self.state[7] == self.state[8] == player
+            ) or (
+                # vertical row
+                self.state[0] == self.state[3] == self.state[6] == player or
+                self.state[1] == self.state[4] == self.state[7] == player or
+                self.state[2] == self.state[5] == self.state[8] == player
+            ) or (
+                # diagonal
+                self.state[0] == self.state[4] == self.state[8] == player or
+                self.state[2] == self.state[4] == self.state[6] == player
+            )
 
 def main():
     print("hi tic tac toe")
@@ -29,11 +103,19 @@ def main():
     while True:
         if board.human_turn:
             board.move_human()
+            board.display()
+            if board.check_winner(PLAYER):
+                print("Player wins")
+                break
         else:
-            board.move_ai()
-        board.display()
-
-        
+            board.move_ai_perfect()
+            board.display()
+            if board.check_winner(AI):
+                print("AI wins")
+                break
+        if board.turn == 9:
+            print("Cat game")
+            break
 
 if __name__ == "__main__":
     main()
